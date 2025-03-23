758656c36a45c0fe9bd2d0bae0081505823fd8aa
Luca Mingarelli <lucamingarelli@me.com>
2025-03-23 21:14:54 +0000

    :rocket: Version 0.1.1 is out!

--- ecbdata/__about__.py
+++ ecbdata/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "ecbdata"
 __about__ = "The ecbdata API allows for easy querying of data from the ECB Data Portal: https://data.ecb.europa.eu/help/data/overview."
-__version__ = '0.0.9'
+__version__ = '0.1.1'
 __author__ = 'Luca Mingarelli'
 __email__ = "luca.mingarelli@ecb.europa.eu"
 __url__ = "https://github.com/LucaMingarelli/ecbdata"
